0169c87d8e800e78629598f8ad86ce29e4d17199
Kapil Gupta <kpgupta98@gmail.com>
2019-11-27 15:55:07 -0800

    m

--- experiments/exp_2_dual_cnn/model.py
+++ experiments/exp_2_dual_cnn/model.py
@@ -40,7 +40,7 @@ class Model_A(nn.Module):
             nn.BatchNorm2d(64),            
             nn.MaxPool2d(kernel_size=2, stride=2))
         self.layer4 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=5, stride=1, padding=2),
+            nn.Conv2d(in_channels=64, out_channels=32, kernel_size=5, stride=1, padding=2),
             nn.ReLU(),
             nn.BatchNorm2d(32),            
             nn.MaxPool2d(kernel_size=2, stride=2))
