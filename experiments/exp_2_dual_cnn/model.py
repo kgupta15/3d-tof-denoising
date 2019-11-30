2804ee7c8a11f3fd010e606865dc9a96dafbadfe
Kapil Gupta <kpgupta98@gmail.com>
2019-11-30 13:46:13 -0800

    model

--- experiments/exp_2_dual_cnn/model.py
+++ experiments/exp_2_dual_cnn/model.py
@@ -106,8 +106,8 @@ class Model_B(nn.Module):
     def forward(self, x, y):
         out = self.layer1(x)
         print('Shape of parameters : {}'.format(y.shape))
-        # out_param = self.param_layer1(y)
-        # print("LayerParam 1 Output Shape : {}".format(out_param.shape))
+        out_param = self.param_layer1(y)
+        print("LayerParam 1 Output Shape : {}".format(out_param.shape))
         print("Layer 1 Output Shape : {}".format(out.shape))
         out = self.layer2(out)
         print("Layer 2 Output Shape : {}".format(out.shape))
