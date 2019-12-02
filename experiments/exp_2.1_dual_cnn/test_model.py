8e221ba911978192ba153ca8eb7398f443a7d7a8
Kapil Gupta <kpgupta98@gmail.com>
2019-12-01 18:12:19 -0800

    testing

--- experiments/exp_2.1_dual_cnn/test_model.py
+++ experiments/exp_2.1_dual_cnn/test_model.py
@@ -30,6 +30,7 @@ params = model_a(torch.from_numpy(img).type(torch.FloatTensor).to(device))
 print(params)
 """
 
+"""
 model_b = Model_B(config_b)
 model_b.to(device)
 os.chdir('../../data')
@@ -39,3 +40,17 @@ img = np.expand_dims(img, axis=0)
 img = np.expand_dims(img, axis=1)
 out_img = model_b(torch.from_numpy(img).type(torch.FloatTensor).to(device), torch.from_numpy(np.random.random((1,1,8,128))).type(torch.FloatTensor).to(device))
 print(out_img.shape)
+"""
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = Model_B(config_b)
+os.chdir('/storage-pod/3d-tof-denoising/experiments/exp_2.1_dual_cnn/runs_b/checkpoints')
+bestckpt_file = 'best_checkpoint.pth.tar'
+ckpt_file = 'checkpoint.pth.tar'
+ckpt = model.load(bestckpt_file)
+model.load_state_dict(ckpt['state_dict']).to(device)
+os.chdir('/storage-pod/3d-tof-denoising/data/FLAT/FLAT/kinect/reflection')
+fdm = np.fromfile('./1520215621736999', dtype=np.int32)
+fdm = Image.fromarray(fdm.reshape((424, 512, 9))[:,:,1])
+transform = transforms.Compose([transforms.ToTensor()]) 
+out = model(transform(fdm).view(1, 1, 424, 512).type(torch.FloatTensor).to(device), torch.from_numpy(param).type(torch.FloatTensor).to(device))
